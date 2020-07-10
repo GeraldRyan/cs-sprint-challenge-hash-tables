c3b38c09a408e0ec84252fb9d6f4fe1bcd4f3320
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-10 11:51:19 -0500

    got flight ticket done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,28 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    hashtable = {}
+    last_port = ""
+    for t in tickets:
+        hashtable[t.source] = t.destination
+        if t.destination == 'NONE':
+            last_port = t.source
 
+    print(hashtable)
+    route = ["NONE"] * (length)
+    route[0] = hashtable['NONE']
+    route[length-2] = last_port
+    for d in range(1, len(tickets)-1, 1):
+        route[d] = hashtable[route[d-1]]
+    print(route)
     return route
+
+
+if __name__ == "__main__":
+    ticket_1 = Ticket("NONE", "PDX")
+    ticket_2 = Ticket("PDX", "DCA")
+    ticket_3 = Ticket("DCA", "NONE")
+
+    tickets = [ticket_1, ticket_2, ticket_3]
+
+    reconstruct_trip(tickets, 3)
