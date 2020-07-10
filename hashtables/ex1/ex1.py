4c54e201daa122b80a7fcfa37b637c60865c120e
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-10 10:26:27 -0500

    got questions answered and weights begun

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,14 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    for w in weights:
+        print(w)
+
     return None
+
+
+if __name__ == "__main__":
+    weights = [4, 6, 10, 15, 16]
+    length = len(weights)
+    limit = 21
+    get_indices_of_item_weights(weights, length, limit)
