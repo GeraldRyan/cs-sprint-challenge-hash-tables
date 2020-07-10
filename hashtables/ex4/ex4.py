d26d688d9961836ee9967b5b2297f1fa6915fb6c
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-10 12:18:56 -0500

    got fourth ex done (skipped third). Thats mvp. now to stretch

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,23 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    hashpos = {}
+    hashneg = {}
+    for item in a:
+        if item > 0:
+            hashpos[item] = item
+        elif item < 0:
+            hashneg[-item] = item
+    # print(hashpos)
+    # print(hashneg)
 
+
+    result = []
+    for item in hashpos:
+        if item in hashneg:
+            result.append(item)
+
+    print("Result", result)
     return result
 
 
